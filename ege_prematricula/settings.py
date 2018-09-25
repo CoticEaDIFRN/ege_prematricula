4652cb763d47110dffd92cb954463f74464a1b38
johnt1000 <jonatas.rodriguess@gmail.com>
2018-09-25 14:35:17 -0300

    [UPD] Primeira versão do APP Processo Seletivo.

--- ege_prematricula/settings.py
+++ ege_prematricula/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'processo_seletivo.apps.ProcessoSeletivoConfig',
     'dominio.apps.DominioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ processo_seletivo/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class ProcessoSeletivo(models.Model):
+    PERIODO = (
+        (1, '1'),
+        (2, '2'),
+    )
+
+    identificacao = models.CharField(_('Identificação'), max_length=256)
+    ano_letivo = models.PositiveIntegerField(_('Ano letivo'))
+    periodo_letivo = models.PositiveIntegerField(_('Período letivo'), choices=PERIODO)
+    conclusao_intercambio = models.DateField(_('Conclusão do intercâmbio'), auto_now=False, auto_now_add=False)
+    matriz_curso = models.CharField(_('Matriz \ Curso'), max_length=256)
+    linha_pesquisa = models.CharField(_('Linha de pesquisa'), max_length=256)
+    aluno_especial = models.BooleanField(_('Aluno especial?'))
+    numero_pasta = models.CharField(_('Número da pasta'),  max_length=100)
+    descricao = models.TextField(_('Descrição'), max_length=2000, null=True, blank=True)
+    observacao = models.TextField(_('Observação'), max_length=2000, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.identificacao
+
+    class Meta:
+        verbose_name = _('Processo seletivo')
+        verbose_name_plural = _('Processos seletivos')
+        ordering = ['identificacao']
