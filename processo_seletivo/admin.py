a59dc5c61481eb133e9d4fe2f9c5b8258d2024c8
johnt1000 <jonatas.rodriguess@gmail.com>
2018-09-26 11:27:20 -0300

    [UPD] Organizando listagem de Processo Seletivo.

--- processo_seletivo/admin.py
+++ processo_seletivo/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-
 from .models import ProcessoSeletivo
 
-admin.site.register(ProcessoSeletivo)
+
+class ProcessoSeletivoAdmin(admin.ModelAdmin):
+    list_display = ['identificacao', 'ano_letivo', 'periodo_letivo', 'polo']
+    search_fields = ['id','identificacao', 'ano_letivo', 'periodo_letivo', 'polo']
+
+
+admin.site.register(ProcessoSeletivo, ProcessoSeletivoAdmin)
