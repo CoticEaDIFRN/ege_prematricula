2ed6358779de6ccd6d2d0502b9b745629b0a281a
johnt1000 <jonatas.rodriguess@gmail.com>
2018-09-27 14:18:27 -0300

    [UPD] Ajuste de form de Processo Seletivo.

--- processo_seletivo/admin.py
+++ processo_seletivo/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 from .models import ProcessoSeletivo
 from .models import Oferta
+from .models import Arquivo
+
 
 class OfertaInline(admin.TabularInline):
     model = Oferta
+    extra = 1
+
+
+class ArquivoInline(admin.TabularInline):
+    model = Arquivo
+    extra = 1
+
 
 class ProcessoSeletivoAdmin(admin.ModelAdmin):
     inlines = [
         OfertaInline,
+        ArquivoInline
     ]
-    # list_display = ['identificacao', 'ano_letivo', 'periodo_letivo', 'polo']
-    # search_fields = ['id','identificacao', 'ano_letivo', 'periodo_letivo', 'polo']
+    # list_display = ['identificacao', 'ano_letivo', 'periodo_letivo']
+    # search_fields = ['id','identificacao', 'ano_letivo', 'periodo_letivo']
 
 
 admin.site.register(ProcessoSeletivo, ProcessoSeletivoAdmin)
--- processo_seletivo/models.py
+++ processo_seletivo/models.py
@@ -7,14 +7,59 @@ class ProcessoSeletivo(models.Model):
         (1, '1º Período'),
         (2, '2º Período'),
     )
-    identificacao = models.CharField(_('Identificação'), max_length=256, help_text='Digite sua identificação')
-    uo = models.CharField(_('Unidade organizacional'),  max_length=100, help_text='Digite a unidade organizacional')
-    numero_ano = models.CharField(_('Número / Ano'),  max_length=100, help_text='Digite o número e o ano do edital')
+    uo = models.CharField(_('Unidade organizacional'),  max_length=100, help_text='Ex.: DG-EAD/IFRN')
+    numero_ano = models.CharField(_('Número / Ano'),  max_length=100, help_text='Digite o número e o ano do edital. Ex.: 36/2018')
     ano_letivo = models.PositiveIntegerField(_('Ano letivo'), help_text='Digite o ano')
     periodo_letivo = models.PositiveIntegerField(_('Período letivo'), choices=PERIODO)
-    url = models.URLField(_('Endereço do edital'),  max_length=300, help_text='Informe o LINK onde está o edital')
-    descricao = models.TextField(_('Descrição'), max_length=2000, null=True, blank=True)
+    url = models.URLField(_('URL'),  max_length=300, help_text='Informe o LINK onde está o edital')
+    descricao = models.CharField(_('Descrição'), max_length=256, null=True, blank=True)
 
+    def __str__(self):
+        return self.identificacao
+
+    @property
+    def identificacao(self):
+        return "Edital %s (%s)" % (self.numero_ano, self.uo)
+
+    class Meta:
+        verbose_name = _('Processo seletivo')
+        verbose_name_plural = _('Processos seletivos')
+
+
+class Oferta(models.Model):
+    processo_seletivo = models.ForeignKey(ProcessoSeletivo, on_delete=models.CASCADE)
+    campus = models.ForeignKey('dominio.Campus', on_delete=models.CASCADE)
+    curso = models.ForeignKey('dominio.Curso', on_delete=models.CASCADE)
+    turno = models.ForeignKey('dominio.Turno', on_delete=models.CASCADE)
+    polo = models.ForeignKey('dominio.Polo', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+    @property
+    def nome(self):
+        return "%s > %s > %s" % (self.campus, self.curso, self.turno)
+
+    class Meta:
+        verbose_name = _('Oferta')
+        verbose_name_plural = _('Ofertas')
+
+
+class Arquivo(models.Model):
+    arquivo = models.FileField(_('Arquivo de importação'), help_text='Informe o arquivo de importação')
+    processo_seletivo = models.ForeignKey(ProcessoSeletivo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.arquivo)
+
+    class Meta:
+        verbose_name = _('Arquivo de importação')
+        verbose_name_plural = _('Arquivos de importações')
+        ordering = ['arquivo']
+
+
+class Selecionado(models.Model):
+    estado_civil = models.ForeignKey('dominio.EstadoCivil', on_delete=models.CASCADE,null=True, blank=True)
     # forma_ingresso = models.ForeignKey('dominio.FormaIngresso', on_delete=models.CASCADE)
     # convenio = models.ForeignKey('dominio.Convenio', on_delete=models.CASCADE,null=True, blank=True)
     # conclusao_intercambio = models.DateField(_('Conclusão do intercâmbio'), auto_now=False, auto_now_add=False)
@@ -23,30 +68,3 @@ class ProcessoSeletivo(models.Model):
     # aluno_especial = models.BooleanField(_('Aluno especial?'))
     # numero_pasta = models.CharField(_('Número da pasta'),  max_length=100)
     # observacao = models.TextField(_('Observação'), max_length=2000, null=True, blank=True)
-
-
-    def __str__(self):
-        return self.identificacao
-
-    class Meta:
-        verbose_name = _('Processo seletivo')
-        verbose_name_plural = _('Processos seletivos')
-        ordering = ['identificacao']
-
-
-class Oferta(models.Model):
-    nome = models.CharField(_('Nome'), max_length=256, help_text='Digite o nome do processo seletivo')
-    processo_seletivo = models.ForeignKey(ProcessoSeletivo, on_delete=models.CASCADE)
-    campus = models.ForeignKey('dominio.Campus', on_delete=models.CASCADE)
-    polo = models.ForeignKey('dominio.Polo', on_delete=models.CASCADE)
-    curso = models.ForeignKey('dominio.Curso', on_delete=models.CASCADE)
-    turno = models.ForeignKey('dominio.Turno', on_delete=models.CASCADE)
-    
-
-    def __str__(self):
-        return self.nome
-
-    class Meta:
-        verbose_name = _('Oferta')
-        verbose_name_plural = _('Ofertas')
-        ordering = ['nome']
